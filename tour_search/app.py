de5f4491fe2d30f8ce42133054d68864ebdf2fac
vasyanch <vasyanch852@gmail.com>
2020-03-29 02:27:54 +0300

    second week

--- tour_search/app.py
+++ tour_search/app.py
@@ -8,17 +8,26 @@ app = Flask(__name__)
 
 @app.route('/')
 def render_index():
-    return render_template('index.html', tours=tours, departures=departures)
+    sorted_tours = default_sort(tours)
+    return render_template('index.html', tours=sorted_tours, departures=departures)
 
 
 @app.route('/departure/<departure>/')
 def render_departure(departure):
-    return render_template('departure.html', tours=tours, departures=departures, departure=departure)
+    departure_tours = default_sort(
+        {tour_id: value for tour_id, value in tours.items() if value['departure'] == departure}
+    )
+    prices = [tour[1]['price'] for tour in departure_tours]
+    nights = [tour[1]['nights'] for tour in departure_tours]
+    return render_template(
+        'departure.html', tours=departure_tours, departures=departures, departure=departure,
+        prices=prices, nights=nights
+    )
 
 
-@app.route('/tour/<int:id>/')
-def render_tour(id):
-    return render_template('tour.html', tour=tours.get(id), departures=departures)
+@app.route('/tour/<int:tour_id>/')
+def render_tour(tour_id):
+    return render_template('tour.html', tour=tours.get(tour_id), departures=departures)
 
 
 @app.errorhandler(404)
@@ -29,6 +38,10 @@ def render_not_found(error):
 @app.errorhandler(500)
 def render_server_error(error):
     return f"server is broken {error.original_exception}", 500
+
+
+def default_sort(any_tours: dict) -> list:
+    return sorted(any_tours.items(), key=lambda x: x[1]['price'])
 
 
 if __name__ == '__main__':
